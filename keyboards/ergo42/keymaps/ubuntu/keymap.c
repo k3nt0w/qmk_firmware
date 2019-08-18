2e0710583caf2acbde02c439aa07043d036a42cb
k3nt0w <k3nt0.watanabe@gmail.com>
2019-08-19 04:13:06 +0900

    hoge

--- keyboards/ergo42/keymaps/ubuntu/keymap.c
+++ keyboards/ergo42/keymaps/ubuntu/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(\
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,   KC_EQL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_QUOT,   KC_GRV, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_LBRC, \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC,   _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,    KC_RSRB, \
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC,   _______ , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,    KC_RSRB, \
     KC_LOWR,  KC_LGUI, KC_ESC,  KC_LALT, CMDCTL,  CMDCTL, KC_SPC,   KC_ENT,  KC_RASE,  KC_RASE, KC_SLSH, KC_LEFT, KC_DOWN,   KC_RGHT \
   ),
 
